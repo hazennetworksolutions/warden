2a90aa44ca1d6046ae60164abd2c822938c9ac1f
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2024-10-21 18:59:34 +0300

    Update 0.4.2.autoscript.sh

--- 0.4.2.autoscript.sh
+++ 0.4.2.autoscript.sh
@@ -184,8 +184,7 @@ wardend init ${MONIKER} --chain-id ${WARDEN_CHAIN_ID}
 
 # Download genesis and addrbook files
 printGreen "6. Downloading genesis and addrbook..."
-sudo wget -O /root/genesis.json.tar.xz https://buenavista-genesis.s3.eu-west-1.amazonaws.com/genesis.json.tar.xz && sudo tar -xJf /root/genesis.json.tar.xz -C /root && sudo rm /root/genesis.json.tar.xz
-sudo mv /root/genesis.json /root/.warden/config/
+curl -Ls https://snapshots.kjnodes.com/warden-testnet/genesis.json > $HOME/.warden/config/genesis.json
 wget -O $HOME/.warden/config/addrbook.json "https://raw.githubusercontent.com/hazennetworksolutions/warden/refs/heads/main/addrbook.json"
 
 # Configure gas prices and ports
